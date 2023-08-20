992c2becd889117d125025c969acc04ed49b85f5
ilyas <ilyasouagag2011@gmail.com>
2023-08-21 00:01:29 +0100

    betty

--- 0-printf.c
+++ 0-printf.c
@@ -6,44 +6,44 @@
  */
 int _printf(const char *format, ...)
 {
-int i = 0;
-int j;
-int count = 0;
-va_list args;
-va_start(args, format);
-if (format == NULL || (format[0] == '%' && format[1] == '\0'))
-return (-1);
-while (format[i])
-{
-if (format[i] != '%')
-{
-_write(format[i]);
-i++;
-count++;
-continue;
-}
-if (format[i + 1] == '%')
-{
-_write('%');
-count++;
-i += 2;
-continue;
-}
-if (format[i + 1] == '\0')
-return (-1);
-j = change(format[i + 1], args);
-if (j == -1 || j != 0)
-i++;
-if (j > 0)
-count += j;
-if (j == 0)
-{
-_write('%');
-count++;
-}
-i++;
-}
-va_end(args);
-return (count);
-}
+	int i = 0;
+	int j;
+	int count = 0;
+	va_list args;
 
+	va_start(args, format);
+	if (format == NULL || (format[0] == '%' && format[1] == '\0'))
+		return (-1);
+	while (format[i])
+	{
+		if (format[i] != '%')
+		{
+			_write(format[i]);
+			i++;
+			count++;
+			continue;
+		}
+		if (format[i + 1] == '%')
+		{
+			_write('%');
+			count++;
+			i += 2;
+			continue;
+		}
+		if (format[i + 1] == '\0')
+			return (-1);
+		j = change(format[i + 1], args);
+		if (j == -1 || j != 0)
+			i++;
+		if (j > 0)
+			count += j;
+		if (j == 0)
+		{
+			_write('%');
+			count++;
+		}
+		i++;
+	}
+	va_end(args);
+	return (count);
+}
