b47066ef9f4f73228de63180a25b485ab55be9b1
ilyas <ilyasouagag2011@gmail.com>
2023-08-21 16:15:41 +0100

    task2 recursion

--- main.c
+++ main.c
@@ -1,22 +1,26 @@
 #include "main.h"
-
-int main()
+/**
+ * main -test function
+ * Return: return 0
+ */
+int main(void)
 {
-    int len, len2,x,y;
-    len = _printf("Let's try to printf a simple sentence.\n");
-    len2 = printf("Let's try to printf a simple sentence.\n");
-    _printf("Length:[%d, %i]\n", len, len);
-    printf("Length:[%d, %i]\n", len2, len2);
-    _printf("Negative:[%d]\n", -762534);
-    printf("Negative:[%d]\n", -762534);
-    _printf("Character:[%c]\n", 'H');
-    printf("Character:[%c]\n", 'H');
-    _printf("String:[%s]\n", "I am a string !");
-    printf("String:[%s]\n", "I am a string !");
-    x = _printf("%b",-98);
-    printf("\n");
-    y = printf("%b",-98);
-    printf("%d\n",x);
-    printf("%d",y);
-    return 0;
+	int len, len2, x, y;
+
+	len = _printf("Let's try to printf a simple sentence.\n");
+	len2 = printf("Let's try to printf a simple sentence.\n");
+	_printf("Length:[%d, %i]\n", len, len);
+	printf("Length:[%d, %i]\n", len2, len2);
+	_printf("Negative:[%d]\n", -762534);
+	printf("Negative:[%d]\n", -762534);
+	_printf("Character:[%c]\n", 'H');
+	printf("Character:[%c]\n", 'H');
+	_printf("String:[%s]\n", "I am a string !");
+	printf("String:[%s]\n", "I am a string !");
+	x = _printf("%b", -98);
+	printf("\n");
+	y = printf("%b", -98);
+	printf("%d\n", x);
+	printf("%d", y);
+	return (0);
 }
